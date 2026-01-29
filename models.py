3d82e525a1f2db3f390e26da8a7feaf5fd5e084f
Ansh Raval <ravalansh@284@gmail.com>
2026-01-29 17:11:24 +0530

    removed the comments from models.py file

--- models.py
+++ models.py
@@ -8,8 +8,6 @@ class User(Base):
     username=Column(String,unique=True,index=True)
     email=Column(String,unique=True,index=True)
     hashed_password=Column(String)
-    # is_active=Column(Boolean,default=True)
-    # is_admin=Column(Boolean,default=False)
     role_links=relationship("UserRole",back_populates="user")
 
 
